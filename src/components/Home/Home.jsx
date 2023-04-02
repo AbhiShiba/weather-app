226e717a8ff0886b032da5909b7b3fb0e9e55bac
abhi shiba <shivarajabhishek@gmail.com>
2023-04-02 08:24:47 +0530

    removed unused import

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react'
 import { WeatherContext } from '../../App'
 import { HomeScreen } from '../HomeScreen/HomeScreen';
-import { Search } from '../Search/Search';
 import './Home.css'
 
 export function Home() {
